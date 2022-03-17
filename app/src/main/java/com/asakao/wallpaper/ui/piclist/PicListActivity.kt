c3f2bf619b5741d80dd505439d8283aa1288495c
asakao <zdshen@chinaums.com>
2022-03-17 15:22:02 +0800

    modify delete action bar

--- app/src/main/java/com/asakao/wallpaper/ui/piclist/PicListActivity.kt
+++ app/src/main/java/com/asakao/wallpaper/ui/piclist/PicListActivity.kt
@@ -1,7 +1,6 @@
 package com.asakao.wallpaper.ui.piclist
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
--- app/src/main/java/com/asakao/wallpaper/ui/piclist/PicListAdapter.kt
+++ app/src/main/java/com/asakao/wallpaper/ui/piclist/PicListAdapter.kt
@@ -5,8 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.asakao.wallpaper.R
