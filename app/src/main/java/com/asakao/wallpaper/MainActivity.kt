4c8150401b54218a16b51803a5cbdd06e7a9ecbb
asakao <zdshen@chinaums.com>
2022-03-20 12:48:11 +0800

    modify change bar color

--- app/src/main/java/com/asakao/wallpaper/MainActivity.kt
+++ app/src/main/java/com/asakao/wallpaper/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.asakao.wallpaper
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -17,6 +19,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val decorView = window.decorView
+        decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        window.statusBarColor = Color.parseColor("#779E9D")
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.getInfo.setOnClickListener {
